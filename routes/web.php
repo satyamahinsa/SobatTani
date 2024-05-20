e0cb42777c404aa5ec1787e033a4808c62841144
Gede Satyamahinsa <satyamahinsaa.pu@gmail.com>
2024-05-20 22:17:09 +0700

    push template views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home']);
+});
+
+Route::get('/kursus', function () {
+    return view('kursus', ['title' => 'Kursus']);
+});
+
+Route::get('/jual-beli', function () {
+    return view('jual-beli', ['title' => 'Jual Beli']);
+});
+
+Route::get('/harga-pasar', function () {
+    return view('harga-pasar', ['title' => 'Harga Pasar']);
+});
+
+Route::get('/titik-kesuburan-tanah', function () {
+    return view('titik-kesuburan-tanah', ['title' => 'Titik Kesuburan Tanah']);
 });
